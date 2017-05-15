bef24a98d115ce5aac4764c939e23be5167a482e
Niels Gregers Johansen <ngjoh@nets.eu>
2017-05-15 19:45:47 +0200

    real code

---
+++ setupTestTenant/modules/download-modules.ps1
@@ -0,0 +1,4 @@
+#Save-Module SharePointPnPPowerShellOnline  -Path "$PSScriptRoot" 
+Save-Module AzureAD  -Path "$PSScriptRoot" 
+Save-Module PSExcel  -Path "$PSScriptRoot" 
+#Save-Module AzureRM -Path "$PSScriptRoot" 
--- setupTestTenant/run.ps1
+++ setupTestTenant/run.ps1
@@ -1,11 +1,26 @@
-# POST method: $req
-$requestBody = Get-Content $req -Raw | ConvertFrom-Json
-$name = $requestBody.name
-
-# GET method: each querystring parameter is its own variable
-if ($req_query_name) 
-{
-    $name = $req_query_name 
+# If hosted in Azure Web Jobs modules are auto loaded
+if ($PSScriptRoot){ 
+    Import-Module ".\modules\azuread\2.0.0.109\azuread.psd1"
+    Import-Module ".\modules\hexa-functions.psm1"
 }
 
-Out-File -Encoding Ascii -FilePath $res -inputObject "Hello $name"
+Get-Hexa $req $res $PSScriptRoot
+
+$account = Connect-AzureAD -Credential $global:credentials -ErrorAction:Stop
+
+$usersCreated = 0
+foreach ($user in $global:request.users) {
+    $name = $user.Replace("@",".")
+    $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+    $PasswordProfile.Password = "Password123456!"
+    $upn = $name + "@" + $global:o365Tenant + ".onmicrosoft.com"
+
+    $aduser = Get-AzureADUser $upn
+    if ($aduser -eq $null){
+        New-AzureADUser -DisplayName "User $name" -PasswordProfile $PasswordProfile -UserPrincipalName $upn -AccountEnabled $true -MailNickName $name 
+        $usersCreated += 1
+    }
+}
+
+
+Set-Hexa $usersCreated
